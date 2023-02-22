cafdfbfa15c403e91860c91efff37b020ba76f70
Benjamin <benjamin.guy.thomas@gmail.com>
2023-02-22 12:36:44 +0100

    Fix click bubbling up to the h1

--- elm/counter/src/Main.elm
+++ elm/counter/src/Main.elm
@@ -1,11 +1,5 @@
 module Main exposing (main)
 
-{-
-   Run with:
-
-     elm-live --host=0.0.0.0 src/Main.elm
--}
-
 import Browser
 import Html exposing (Html, button, div, h1, input, span, text)
 import Html.Attributes exposing (disabled, readonly, style, value)
@@ -31,12 +25,20 @@ cssDisableSelection =
 view : Model -> Html Msg
 view model =
     div []
-        [ h1 [] [ text "Counter example" ]
-        , span
-            cssDisableSelection
-            [ input [ value (String.fromInt model), readonly True, disabled True ] []
+        [ div []
+            [ h1 []
+                [ text "Counter example"
+                ]
+
+            -- Extra div required to prevent click events bubbling up to the h1
+            , div []
+                [ span
+                    cssDisableSelection
+                    [ input [ value (String.fromInt model), readonly True, disabled True ] []
+                    ]
+                , button [ onClick Inc ] [ text "count" ]
+                ]
             ]
-        , button [ onClick Inc ] [ text "count" ]
         ]
 
 
