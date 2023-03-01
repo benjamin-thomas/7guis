8d09f8691447c2bb81daef793d6cf3b79742f120
Benjamin <benjamin.guy.thomas@gmail.com>
2023-03-01 05:38:03 +0100

    Use a "better" model

--- elm/temp_converter/src/Main.elm
+++ elm/temp_converter/src/Main.elm
@@ -1,7 +1,7 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Html, div, input, label, p, span, text)
+import Html exposing (Html, div, input, label, span, text)
 import Html.Attributes exposing (value)
 import Html.Events exposing (onInput)
 
@@ -20,31 +20,33 @@ toFahrenheit c =
     c * 9 / 5 + 32
 
 
-toNothingIfBlank : String -> Maybe String
-toNothingIfBlank s =
-    if String.trim s == "" then
-        Nothing
-
-    else
-        Just s
-
-
 maybeConvert : (Float -> Float) -> String -> Maybe String
 maybeConvert f str =
-    Maybe.map (f >> String.fromFloat) (String.toFloat str)
+    String.toFloat str |> Maybe.map (f >> String.fromFloat)
 
 
 
 -- MODEL
 
 
-type alias Model =
-    { celsius : Maybe String, fahrenheit : Maybe String }
+type Celsius
+    = Celsius String
+
+
+type Fahrenheit
+    = Fahrenheit String
+
+
+type Model
+    = NoUserInput
+    | Converted Celsius Fahrenheit
+    | BadInputCelsius Celsius
+    | BadInputFahrenheit Fahrenheit
 
 
 init : Model
 init =
-    { celsius = Nothing, fahrenheit = Nothing }
+    NoUserInput
 
 
 
@@ -57,49 +59,75 @@ type Msg
 
 
 update : Msg -> Model -> Model
-update msg model =
+update msg _ =
     case msg of
-        CelsiusChanged s ->
-            { model | celsius = toNothingIfBlank s, fahrenheit = maybeConvert toFahrenheit s }
+        CelsiusChanged celsius ->
+            case maybeConvert toFahrenheit celsius of
+                Nothing ->
+                    BadInputCelsius (Celsius celsius)
 
-        FahrenheitChanged s ->
-            { model | fahrenheit = toNothingIfBlank s, celsius = maybeConvert toCelsius s }
+                Just fahrenheit ->
+                    Converted (Celsius celsius) (Fahrenheit fahrenheit)
+
+        FahrenheitChanged fahrenheit ->
+            case maybeConvert toCelsius fahrenheit of
+                Nothing ->
+                    BadInputFahrenheit (Fahrenheit fahrenheit)
+
+                Just celsius ->
+                    Converted (Celsius celsius) (Fahrenheit fahrenheit)
 
 
 
 -- VIEW
 
 
-warnOnInvalidInput : String -> Maybe String -> Html msg
-warnOnInvalidInput name ms =
-    case Maybe.andThen toNothingIfBlank ms of
-        Nothing ->
+valueFor : Model -> { celsius : String, fahrenheit : String }
+valueFor model =
+    case model of
+        NoUserInput ->
+            { celsius = "", fahrenheit = "" }
+
+        Converted (Celsius celsius) (Fahrenheit fahrenheit) ->
+            { celsius = celsius, fahrenheit = fahrenheit }
+
+        BadInputCelsius (Celsius celsius) ->
+            { celsius = celsius, fahrenheit = "" }
+
+        BadInputFahrenheit (Fahrenheit fahrenheit) ->
+            { celsius = "", fahrenheit = fahrenheit }
+
+
+showInvalid : Model -> Html msg
+showInvalid model =
+    case model of
+        BadInputCelsius _ ->
+            text "Celsius: bad input!"
+
+        BadInputFahrenheit _ ->
+            text "Fahrenheit: bad input!"
+
+        NoUserInput ->
             text ""
 
-        Just s ->
-            case String.toFloat s of
-                Nothing ->
-                    p [] [ text ("Invalid " ++ name ++ " input") ]
-
-                Just _ ->
-                    text ""
+        Converted _ _ ->
+            text ""
 
 
 view : Model -> Html Msg
 view model =
     div []
         [ span []
-            [ input [ value (Maybe.withDefault "" model.celsius), onInput CelsiusChanged ] []
+            [ input [ value (valueFor model).celsius, onInput CelsiusChanged ] []
             , label [] [ text "Celsius" ]
             ]
         , span [] [ text " = " ]
         , span []
-            [ input [ value (Maybe.withDefault "" model.fahrenheit), onInput FahrenheitChanged ] []
+            [ input [ value (valueFor model).fahrenheit, onInput FahrenheitChanged ] []
             , label [] [ text "Fahrenheit" ]
             ]
         , div []
-            [ warnOnInvalidInput "celsius" model.celsius
-            , warnOnInvalidInput "fahrenheit" model.fahrenheit
+            [ showInvalid model
             ]
         ]
 
