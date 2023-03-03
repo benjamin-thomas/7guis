4213ca3787d61c4c420b8bed9f1eef05bd395777
Benjamin <benjamin.guy.thomas@gmail.com>
2023-03-03 18:44:48 +0100

    Handle click

--- elm/flight_booker/src/Main.elm
+++ elm/flight_booker/src/Main.elm
@@ -3,9 +3,23 @@ module Main exposing (main)
 import Browser
 import Date exposing (Date, fromCalendarDate, fromIsoString, toIsoString)
 import Html exposing (Html, button, div, input, option, select, text)
-import Html.Attributes exposing (selected, style, value)
-import Html.Events exposing (onInput)
+import Html.Attributes exposing (disabled, selected, style, value)
+import Html.Events exposing (onClick, onInput)
 import Time exposing (Month(..))
+
+
+
+{-
+   BUG/FIXME:
+       - make departure invalid
+       - make return invalid
+       - make return valid
+       - make departure valid
+       - IsAfterReturn state is not detected
+
+   POSSIBLE SOLUTION:
+       Handle the form change as a whole, not per input
+-}
 
 
 type Departure
@@ -16,9 +30,18 @@ type Return
     = Return Date
 
 
+type DepartureErr
+    = InvalidDepartureDate String
+
+
+type ReturnErr
+    = InvalidReturnDate String
+    | IsAfterReturn String
+
+
 type Model
-    = OneWay (Result String Departure)
-    | RoundTrip (Result String Departure) (Result String Return)
+    = OneWay (Result DepartureErr Departure)
+    | RoundTrip (Result DepartureErr Departure) (Result ReturnErr Return)
 
 
 init : Model
@@ -30,6 +53,7 @@ type Msg
     = FlightTypeChanged String
     | DepartureChanged String
     | ReturnChanged String
+    | BookClicked
 
 
 departureToReturn : Departure -> Return
@@ -37,9 +61,16 @@ departureToReturn (Departure d) =
     Return d
 
 
-errWith : String -> Result String Date
+departureToReturnErr : DepartureErr -> ReturnErr
+departureToReturnErr err =
+    case err of
+        InvalidDepartureDate d ->
+            InvalidReturnDate d
+
+
+errWith : String -> Result DepartureErr Date
 errWith s =
-    fromIsoString s |> Result.mapError (always s)
+    fromIsoString s |> Result.mapError (always (InvalidDepartureDate s))
 
 
 update : Msg -> Model -> Model
@@ -48,7 +79,7 @@ update msg model =
         FlightTypeChanged _ ->
             case model of
                 OneWay md ->
-                    RoundTrip md (Result.map departureToReturn md)
+                    RoundTrip md (md |> Result.map departureToReturn |> Result.mapError departureToReturnErr)
 
                 RoundTrip md _ ->
                     OneWay md
@@ -65,7 +96,7 @@ update msg model =
             case model of
                 RoundTrip dep _ ->
                     let
-                        res : Result String Date
+                        res : Result ReturnErr Date
                         res =
                             let
                                 err =
@@ -75,15 +106,15 @@ update msg model =
                                 ( Ok r, Ok (Departure d) ) ->
                                     if Date.compare r d == LT then
                                         -- Bad user input, return cannot happen before departure!
-                                        Err s
+                                        Err (IsAfterReturn s)
 
                                     else
-                                        err
+                                        err |> Result.mapError departureToReturnErr
 
                                 _ ->
-                                    err
+                                    err |> Result.mapError departureToReturnErr
 
-                        ret : Result String Return
+                        ret : Result ReturnErr Return
                         ret =
                             res |> Result.map Return
                     in
@@ -92,6 +123,9 @@ update msg model =
                 OneWay _ ->
                     -- FIXME: satisfy the compiler, impossible state!
                     init
+
+        BookClicked ->
+            Debug.todo "BookClicked"
 
 
 txtOneWay : String
@@ -119,20 +153,23 @@ flightOption txt model =
     option [ selected (isSelected txt model) ] [ text txt ]
 
 
-departureValue : Result String Departure -> String
+departureValue : Result DepartureErr Departure -> String
 departureValue md =
     case md of
-        Err s ->
+        Err (InvalidDepartureDate s) ->
             s
 
         Ok (Departure d) ->
             toIsoString d
 
 
-returnValue : Result String Return -> String
+returnValue : Result ReturnErr Return -> String
 returnValue md =
     case md of
-        Err s ->
+        Err (InvalidReturnDate s) ->
+            s
+
+        Err (IsAfterReturn s) ->
             s
 
         Ok (Return d) ->
@@ -149,19 +186,70 @@ errStyle e =
             style "" ""
 
 
+invalidDepartureWarning : Result DepartureErr value -> Html msg
+invalidDepartureWarning dep =
+    case dep of
+        Ok _ ->
+            text ""
+
+        Err (InvalidDepartureDate _) ->
+            text "The departure date is not valid!"
+
+
+invalidReturnWarning : Result ReturnErr value -> Html msg
+invalidReturnWarning ret =
+    case ret of
+        Ok _ ->
+            text ""
+
+        Err (InvalidReturnDate _) ->
+            text "The return date is not valid!"
+
+        Err (IsAfterReturn _) ->
+            text "Return cannot happen before departure!"
+
+
 viewDateInputs : Model -> Html Msg
 viewDateInputs model =
     case model of
         -- I move away frome the requirements slightly here.
         -- Returning 2 inputs for one way seems kludgy to me.
         OneWay dep ->
-            div [] [ input [ errStyle dep, value (departureValue dep), onInput DepartureChanged ] [] ]
+            div []
+                [ input [ errStyle dep, value (departureValue dep), onInput DepartureChanged ] []
+                , invalidDepartureWarning dep
+                ]
 
         RoundTrip dep ret ->
             div []
-                [ div [] [ input [ errStyle dep, value (departureValue dep), onInput DepartureChanged ] [] ]
-                , div [] [ input [ errStyle ret, value (returnValue ret), onInput ReturnChanged ] [] ]
+                [ div []
+                    [ input [ errStyle dep, value (departureValue dep), onInput DepartureChanged ] []
+                    , invalidDepartureWarning dep
+                    ]
+                , div []
+                    [ input [ errStyle ret, value (returnValue ret), onInput ReturnChanged ] []
+                    , invalidReturnWarning ret
+                    ]
                 ]
+
+
+anyErrors : Model -> Bool
+anyErrors model =
+    case model of
+        OneWay (Err _) ->
+            True
+
+        RoundTrip (Err _) _ ->
+            True
+
+        RoundTrip _ (Err _) ->
+            True
+
+        OneWay (Ok _) ->
+            False
+
+        RoundTrip (Ok _) (Ok _) ->
+            False
 
 
 view : Model -> Html Msg
@@ -174,7 +262,7 @@ view model =
                 ]
             ]
         , viewDateInputs model
-        , div [] [ button [] [ text "Book" ] ]
+        , div [] [ button [ disabled (anyErrors model), onClick BookClicked ] [ text "Book" ] ]
         ]
 
 
