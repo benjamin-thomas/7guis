c1084851c947b0876b2b4609c6c641f38fff2578
Benjamin <benjamin.guy.thomas@gmail.com>
2023-03-19 15:05:22 +0100

    Detect circle click or add

--- elm/circle_drawer/src/Main.elm
+++ elm/circle_drawer/src/Main.elm
@@ -31,7 +31,7 @@ type alias Circle =
 
 
 type alias Model =
-    { mousePos : MousePos, circles : List Circle }
+    { mousePos : MousePos, circles : List Circle, interSect : Bool }
 
 
 init : Model
@@ -40,32 +40,96 @@ init =
     , circles =
         [ { cx = 50, cy = 50, r = 40, stroke = "black", fill = "transparent" }
         ]
+    , interSect = False
     }
 
 
 type Msg
     = MouseMove MousePos
-    | AddCircle
+    | SelectOrAddCircle
+
+
+isIntersect : MousePos -> Circle -> Bool
+isIntersect pos c =
+    let
+        dx =
+            c.cx - pos.x |> toFloat
+
+        dy =
+            c.cy - pos.y |> toFloat
+
+        dist =
+            sqrt (dx * dx + dy * dy)
+    in
+    dist < toFloat c.r
+
+
+findCircle : MousePos -> List Circle -> { before : List Circle, found : Maybe Circle, after : List Circle }
+findCircle pos lst =
+    -- NOTE: I don't handle overlapping circles very well (the drawing order may not be preserved)
+    let
+        inner pos_ lst_ acc =
+            case lst_ of
+                [] ->
+                    { before = acc, found = Nothing, after = [] }
+
+                x :: xs ->
+                    if isIntersect pos_ x then
+                        { before = acc, found = Just x, after = xs }
+
+                    else
+                        inner pos_ xs (x :: acc)
+    in
+    inner pos lst []
 
 
 update : Msg -> Model -> Model
 update msg model =
     case msg of
         MouseMove pos ->
-            { model | mousePos = pos }
-
-        AddCircle ->
             let
-                newCircle : Circle
-                newCircle =
-                    { cx = model.mousePos.x
-                    , cy = model.mousePos.y
-                    , r = 40
-                    , stroke = "black"
-                    , fill = "transparent"
-                    }
+                dx =
+                    50 - pos.x |> toFloat
+
+                dy =
+                    50 - pos.y |> toFloat
+
+                dist =
+                    sqrt (dx * dx + dy * dy)
             in
-            { model | circles = newCircle :: model.circles }
+            { model | mousePos = pos, interSect = dist < toFloat 40 }
+
+        SelectOrAddCircle ->
+            let
+                pos =
+                    model.mousePos
+
+                search =
+                    findCircle pos model.circles
+
+                newModel =
+                    case search.found of
+                        Nothing ->
+                            let
+                                newCircle : Circle
+                                newCircle =
+                                    { cx = model.mousePos.x
+                                    , cy = model.mousePos.y
+                                    , r = 40
+                                    , stroke = "black"
+                                    , fill = "transparent"
+                                    }
+                            in
+                            { model | circles = newCircle :: model.circles }
+
+                        Just circle ->
+                            let
+                                newCircle =
+                                    { circle | fill = "orange" }
+                            in
+                            { model | circles = search.before ++ newCircle :: search.after }
+            in
+            newModel
 
 
 decodeMousePos : D.Decoder MousePos
@@ -92,14 +156,21 @@ view model =
     H.div
         []
         [ H.h1 [] [ H.text "Circle Drawer" ]
-        , H.pre [ A.style "white-space" "pre-wrap" ] [ H.text (Debug.toString model.mousePos) ]
+        , H.pre [ A.style "white-space" "pre-wrap" ]
+            [ H.text <|
+                Debug.toString model.mousePos
+                    ++ " "
+                    ++ Debug.toString model.interSect
+            ]
+        , H.pre [ A.style "white-space" "pre-wrap" ]
+            [ H.text <| String.join "\n" <| List.map (\c -> Debug.toString c) model.circles ]
         , H.div [ A.style "text-align" "center" ]
             [ svg
                 [ width "400px"
                 , height "400px"
                 , A.style "border" "1px solid red"
                 , E.on "mousemove" (D.map MouseMove decodeMousePos)
-                , E.onClick AddCircle
+                , E.onClick SelectOrAddCircle
                 ]
                 (List.map viewCircle model.circles)
             ]
