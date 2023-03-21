30444c84f1adbe9d5189935ac563440c99e60b32
Benjamin <benjamin.guy.thomas@gmail.com>
2023-03-21 20:35:16 +0100

    Handle circle resize

--- elm/circle_drawer/src/Main.elm
+++ elm/circle_drawer/src/Main.elm
@@ -18,6 +18,8 @@ import Svg.Attributes exposing (cx, cy, fill, height, r, stroke, width)
 type alias MousePos =
     { x : Int
     , y : Int
+    , absoluteX : Int
+    , absoluteY : Int
     }
 
 
@@ -30,16 +32,20 @@ type alias Circle =
 
 
 type alias Model =
-    { mousePos : MousePos, circles : List Circle, selected : Maybe Circle }
+    { mousePos : MousePos, circles : List Circle, selected : Maybe ( Circle, MousePos ), isMenuOpen : Bool }
 
 
 init : Model
 init =
-    { mousePos = { x = 0, y = 0 }
+    { mousePos = { x = 0, y = 0, absoluteX = 0, absoluteY = 0 }
     , circles =
         [ { cx = 50, cy = 50, r = 40, stroke = "black" }
         ]
+
+    -- , selected = Just ( { cx = 50, cy = 50, r = 40, stroke = "black" }, { clientX = 797, clientY = 180, x = 45, y = 43 } )
+    -- , isMenuOpen = True
     , selected = Nothing
+    , isMenuOpen = False
     }
 
 
@@ -47,6 +53,7 @@ type Msg
     = MouseMove MousePos
     | SelectOrAddCircle
     | MouseRightClicked
+    | RadiusChanged String
 
 
 isIntersect : MousePos -> Circle -> Bool
@@ -97,7 +104,7 @@ update msg model =
                 search =
                     findCircle pos model.circles
 
-                newModel =
+                withSelectedOrWithNewCircle =
                     case search.found of
                         Nothing ->
                             let
@@ -117,24 +124,52 @@ update msg model =
                         Just newCircle ->
                             { model
                                 | circles = search.before ++ newCircle :: search.after
-                                , selected = Just newCircle
+                                , selected = Just ( newCircle, model.mousePos )
                             }
             in
-            newModel
+            if model.isMenuOpen then
+                { model | isMenuOpen = False }
+
+            else
+                withSelectedOrWithNewCircle
 
         MouseRightClicked ->
-            let
-                _ =
-                    Debug.log "MouseRightClicked" "triggered!"
-            in
-            model
+            { model | isMenuOpen = True }
+
+        RadiusChanged s ->
+            case ( model.isMenuOpen, model.selected ) of
+                ( True, Just ( circle, pos ) ) ->
+                    let
+                        newCircle =
+                            s
+                                |> String.toInt
+                                |> Maybe.map (\n -> { circle | r = n })
+                                |> Maybe.withDefault circle
+
+                        newCircles =
+                            model.circles
+                                |> List.map
+                                    (\c ->
+                                        if c == circle then
+                                            newCircle
+
+                                        else
+                                            c
+                                    )
+                    in
+                    { model | selected = Just ( newCircle, pos ), circles = newCircles }
+
+                _ ->
+                    model
 
 
 decodeMousePos : D.Decoder MousePos
 decodeMousePos =
-    D.map2 MousePos
+    D.map4 MousePos
         (D.at [ "offsetX" ] D.int)
         (D.at [ "offsetY" ] D.int)
+        (D.at [ "clientX" ] D.int)
+        (D.at [ "clientY" ] D.int)
 
 
 mouseRightClickDecoder : D.Decoder Msg
@@ -155,9 +190,50 @@ alwaysPreventDefault msg =
     ( msg, True )
 
 
+showMenu : Bool -> Maybe ( Circle, MousePos ) -> Html Msg
+showMenu isMenuOpen selected =
+    case ( isMenuOpen, selected ) of
+        ( True, Just ( circle, circlePos ) ) ->
+            H.span
+                [ A.style "position" "absolute"
+                , A.style "top" (String.fromInt (circlePos.absoluteY + 80) ++ "px")
+                , A.style "left" (String.fromInt circlePos.absoluteX ++ "px")
+                , A.style "background" "red"
+                , A.style "padding" "10px 30px"
+                ]
+                [ H.div
+                    [ A.style "display" "flex"
+                    , A.style "flex-direction" "column"
+                    ]
+                    [ H.label []
+                        [ H.text <|
+                            "Adjust diameter of circle at ("
+                                ++ String.fromInt circle.cx
+                                ++ ", "
+                                ++ String.fromInt circle.cy
+                                ++ ")"
+                        ]
+                    , H.input
+                        [ A.type_ "range"
+                        , A.min "10"
+                        , A.max "100"
+                        , A.value (String.fromInt circle.r)
+                        , E.onInput RadiusChanged
+                        ]
+                        []
+                    ]
+                ]
+
+        _ ->
+            H.text ""
+
+
 view : Model -> Html Msg
 view model =
     let
+        selectedCircle =
+            model.selected |> Maybe.map Tuple.first
+
         viewCircle : Circle -> Svg Msg
         viewCircle c =
             circle
@@ -166,7 +242,7 @@ view model =
                 , r (String.fromInt c.r)
                 , stroke c.stroke
                 , fill
-                    (if model.selected == Just c then
+                    (if selectedCircle == Just c then
                         "orange"
 
                      else
@@ -186,6 +262,8 @@ view model =
                 Debug.toString model.mousePos
                     ++ " "
                     ++ Debug.toString model.selected
+                    ++ " "
+                    ++ Debug.toString model.isMenuOpen
             ]
         , H.pre [ A.style "white-space" "pre-wrap" ]
             [ H.text <| String.join "\n" <| List.map (\c -> Debug.toString c) model.circles ]
@@ -198,6 +276,7 @@ view model =
                 , E.onClick SelectOrAddCircle
                 ]
                 (List.map viewCircle model.circles)
+            , showMenu model.isMenuOpen model.selected
             ]
         ]
 
