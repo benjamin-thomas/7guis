8027952ad2a8ad71da30ec20c69e942926e84446
Benjamin <benjamin.guy.thomas@gmail.com>
2023-05-06 15:31:48 +0200

    Java/Swing: remove GUI designer stuff

--- java/Swing/counter/src/main/java/org/example/App.java
+++ java/Swing/counter/src/main/java/org/example/App.java
@@ -1,19 +1,49 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class App {
-    private JPanel mainPanel;
-    private JButton btn;
-    private JTextField counterField;
-
-    private int counter = 0;
+    private final JPanel mainPanel;
 
     public App() {
+        AtomicInteger counter = new AtomicInteger();
+        mainPanel = new JPanel();
+        mainPanel.setLayout(new GridBagLayout());
+        mainPanel.setPreferredSize(new Dimension(400, 300));
+
+        JPanel line = new JPanel();
+        line.setLayout(new BoxLayout(line, BoxLayout.X_AXIS));
+
+        var counterField = new JTextField();
+        int commonHeight = counterField.getPreferredSize().height;
+        counterField.setText(String.valueOf(counter.get()));
+        counterField.setEnabled(false);
+        counterField.setPreferredSize(new Dimension(150, commonHeight));
+        counterField.setMaximumSize(new Dimension(Integer.MAX_VALUE, commonHeight)); // Set maximum height
+
+        line.add(counterField);
+        line.add(Box.createRigidArea(new Dimension(5, 0)));
+
+        var btn = new JButton("Count");
+        btn.setMnemonic('c');
+        btn.setFocusPainted(false);
         btn.addActionListener(e -> {
-            counter++;
-            counterField.setText(String.valueOf(counter));
+            int count = counter.incrementAndGet();
+            counterField.setText(String.valueOf(count));
         });
+
+        // Work around weird height issue related to the native look and field (btn height is off by 1-2 px)
+        btn.setMaximumSize(
+                new Dimension(
+                        btn.getMaximumSize().width,
+                        commonHeight + 2
+                )
+        );
+        line.add(btn);
+
+        mainPanel.add(line);
     }
 
     public static void run() {
